9ab99d025b9b60468e9509e119b6e7d7ef2a2e27
Elechim01 <mikymanny015@gmail.com>
2021-01-29 12:22:04 +0100

    Initial Commit

---
+++ NearMeAppSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NearMeAppSwift
+//
+//  Created by Michele Manniello on 29/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NearMeAppSwift/NearMeAppSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearMeAppSwiftApp.swift
+//  NearMeAppSwift
+//
+//  Created by Michele Manniello on 29/01/21.
+//
+
+import SwiftUI
+
+@main
+struct NearMeAppSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
