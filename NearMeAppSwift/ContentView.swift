c11d5fe6a8c9854ec0a4db156e60986786a51bf2
Elechim01 <mikymanny015@gmail.com>
2021-01-29 17:31:37 +0100

    primo commit

--- NearMeAppSwift/ContentView.swift
+++ NearMeAppSwift/ContentView.swift
@@ -6,11 +6,57 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @State private var search : String = ""
+    @ObservedObject var locationmanager = LocationManager()
+    @State private var landmarks: [Landmark] = [Landmark]()
+    @State private var tapped: Bool = false
+    
+    private func getNearBylandmarks(){
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = search
+        let search = MKLocalSearch(request: request)
+        search.start { (response, error) in
+            if let response = response{
+                let mapItems = response.mapItems
+                self.landmarks = mapItems.map({
+                    Landmark(placemark: $0.placemark)
+                })
+                
+            }
+        }
+    }
+    func calculateOffset() -> CGFloat {
+        if self.landmarks.count > 0 && !self.tapped{
+            return UIScreen.main.bounds.size.height - UIScreen.main.bounds.size.height / 4
+        }else if self.tapped{
+            return 100
+        }else{
+            return UIScreen.main.bounds.size.height
+        }
+    }
+    
     var body: some View {
-        Text("Hello, world!")
+        ZStack(alignment:.top){
+            MapView(landmarks: landmarks)
+            TextField("Search",text: $search,onEditingChanged: { _ in
+            }){
+//                commit
+                self.getNearBylandmarks()
+            }.textFieldStyle(RoundedBorderTextFieldStyle())
             .padding()
+            .offset(y:44)
+            PlaceListView(landmarks: self.landmarks) {
+//                on tap
+                self.tapped.toggle()
+                print("cliccato")
+//                do something
+            }.animation(.spring())
+            .offset(y:calculateOffset())
+            
+        }
     }
 }
 
---
+++ NearMeAppSwift/Models/LandMark.swift
@@ -0,0 +1,24 @@
+//
+//  LandMark.swift
+//  NearMeAppSwift
+//
+//  Created by Michele Manniello on 29/01/21.
+//
+
+import Foundation
+import MapKit
+struct Landmark {
+    let placemark: MKPlacemark
+    var id : UUID{
+        return UUID()
+    }
+    var name: String{
+        self.placemark.name ?? ""
+    }
+    var title: String{
+        self.placemark.title ?? ""
+    }
+    var coordinate: CLLocationCoordinate2D{
+        self.placemark.coordinate
+    }
+}
